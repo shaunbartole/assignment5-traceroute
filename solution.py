2f13c0bd54eb86fc1343dd50410462224f7f967a
shaunbartole <82050408+shaunbartole@users.noreply.github.com>
2022-04-22 18:22:17 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -81,7 +81,7 @@ def get_route(hostname):
 
             #Fill in start
             # Make a raw socket named mySocket
-            icmp = socket.getprotobyname("icmp")
+            icmp = getprotobyname("icmp")
             mySocket = socket(AF_INET,SOCK_RAW,icmp)
             #Fill in end
             mySocket.setsockopt(IPPROTO_IP, IP_TTL, struct.pack('I', ttl))
