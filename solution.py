65e2781c7debaa9360b2450ce81b9e1f7acb57de
shaunbartole <82050408+shaunbartole@users.noreply.github.com>
2022-04-23 12:46:27 -0400

    test

--- solution.py
+++ solution.py
@@ -154,7 +154,10 @@ def get_route(hostname):
 
                     #You should add your responses to your lists here and return your list if your destination IP is met
                     #rtt = str(round((t - timeSent)*1000)) +"ms"
-                    tracelist1.append([str(ttl), rtt, destAddr])
+                    if destAddr :
+                     tracelist1.append([str(ttl), rtt, str(addr[0]), sourceHostname])
+
+                    #tracelist1.append([str(ttl), rtt, destAddr])
                     #tracelist2.append(tracelist1[-1])
                     print(tracelist2)
                     return tracelist2
